1f9d02d3850170b44cb4296a25000f7c940cd0cd
Joaquín Aguirre <rasengan125@hotmail.com>
2020-09-28 21:16:33 -0500

    cambio de funcion read OFF

--- openGL/fileReader.h
+++ openGL/fileReader.h
@@ -114,6 +114,91 @@ void readOFF(string ruta, Superficie* M) {
 	string line;
 
 	int numVertices, numCaras, numAristas;
+	float a, b, c, d, e, f, g, h;
+
+	if (file.is_open()) {
+		getline(file, line); // OFF
+		file >> numVertices >> numCaras >> numAristas;
+
+		M->verticesTotal = numVertices * 3;
+		M->vertices = new float[M->verticesTotal];
+
+		bool identified = false;
+
+		for(int i = 0; i < numVertices; i++) {
+			if (!identified) {
+				string line1;
+				getline(file, line1);
+				stringstream ss(line1);
+
+				vector<float> auxVec;
+				float auxFloat;
+				while (ss >> auxFloat){
+					auxVec.push_back(auxFloat);
+					cout << auxFloat << endl;
+				}
+
+				if (auxVec.size() == 3) M->tipo = "3D";
+				if (auxVec.size() == 6) M->tipo = "3DC";
+				if (auxVec.size() == 8) M->tipo = "3DT";
+
+				for(int j = 0; j < auxVec.size(); j++) M->vertices[j] = auxVec[j];
+
+				identified = true;
+			}
+			else {
+				if (M->tipo == "3D") {
+					file >> a >> b >> c;
+					M->vertices[3 * i] = a;
+					M->vertices[3 * i + 1] = b;
+					M->vertices[3 * i + 2] = c;
+				}
+				if (M->tipo == "3DC") {
+					file >> a >> b >> c >> d >> e >> f;
+					M->vertices[6 * i] = a;
+					M->vertices[6 * i + 1] = b;
+					M->vertices[6 * i + 2] = c;
+					M->vertices[6 * i + 3] = d;
+					M->vertices[6 * i + 4] = e;
+					M->vertices[6 * i + 5] = f;
+				}
+				if (M->tipo == "3DT") {
+					file >> a >> b >> c >> d >> e >> f >> g >> h;
+					M->vertices[8 * i] = a;
+					M->vertices[8 * i + 1] = b;
+					M->vertices[8 * i + 2] = c;
+					M->vertices[8 * i + 3] = d;
+					M->vertices[8 * i + 4] = e;
+					M->vertices[8 * i + 5] = f;
+					M->vertices[8 * i + 6] = g;
+					M->vertices[8 * i + 7] = h;
+				}
+			}
+		}
+
+		if (M->tipo == "3D" && numCaras == 0) {
+			M->tipo = "2D";
+		}
+
+		if (M->tipo != "2D") {
+			M->indicesTotal = numCaras * 3;
+			M->indices = new unsigned int[M->indicesTotal];
+
+			for (int i = 0; i < numCaras; i++) {
+				file >> a >> b >> c >> d;
+				M->indices[3 * i] = b;
+				M->indices[3 * i + 1] = c;
+				M->indices[3 * i + 2] = d;
+			}
+		}
+	}
+
+	file.close();
+}
+	ifstream file(ruta);
+	string line;
+
+	int numVertices, numCaras, numAristas;
 	float a, b, c;
 
 	if (file.is_open()) {
